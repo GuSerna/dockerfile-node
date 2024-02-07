65c6f8ef6b31500d687bf7ce11cf586a957c2791
Gustavo Serna Moraes <gustavo.moraes@vertem.com>
2024-02-06 22:45:07 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+const { Repository } = require('./repository');
 const express = require('express')
 const app = express()
 const port = 3000
 
-const config = {
-    host: 'db',
-    database:'nodedb'
-};
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
-
-const sql = `INSERT INTO people(name) values('Gustavo')`
-connection.query(sql)
-connection.end()
-
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
-})
-
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+app.get('/', async (_, res) => {
+    const selectSql = `SELECT * FROM people`;
+    const people = await Repository.query(selectSql);
+  
+    const title = '<h1>Full Cycle Rocks!</h1>';
+    const list = `
+      <ul>
+        ${people.map(p => `<li>${p.name}</li>`).join('')}
+      </ul>
+    `;
+  
+    res.send(title + list);
+  });
+  
+  app.listen(3000, () => {
+    console.log('Running on port 3000');
+  
+    const createSql = `
+      CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50), PRIMARY KEY (id));
+    `;
+    Repository.query(createSql);
+  
+    const insertSql = `
+      INSERT INTO people (name) values ('Gustavo'), ('FullCycle');
+    `;
+    Repository.query(insertSql);
+  });
---
+++ repository.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+async function query(sql) {
+    const conn = mysql.createConnection(config);
+
+    const queryPromise = new Promise((resolve, reject) => {
+        conn.query(sql, function (error, results) {
+            if (error) reject(error);
+
+            resolve(results)
+        })
+    })
+    const queryResults = await queryPromise;
+    conn.end();
+    return queryResults;
+}
+
+const Repository = {
+    query
+}
+
+module.exports = { Repository }
